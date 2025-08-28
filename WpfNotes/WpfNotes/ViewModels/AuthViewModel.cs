b5c1c7fd0245385bed770901cf9525213ed1ebd2
Irina <irishka.klimovich.2002@mail.ru>
2025-08-28 10:25:24 +0300

    move api models in the folder

--- WpfNotes/WpfNotes/ViewModels/AuthViewModel.cs
+++ WpfNotes/WpfNotes/ViewModels/AuthViewModel.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
-using WpfNotes.Models.Auth;
+using WpfNotes.Models;
 
 namespace WpfNotes.ViewModels
 {
