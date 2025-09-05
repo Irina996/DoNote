daf7dcca44dcc2efd3ff400662a73263c37e2a0f
Irina <irishka.klimovich.2002@mail.ru>
2025-09-05 15:03:46 +0300

    refactor apiService

---
+++ WpfNotes/WpfNotes/ApiModels/TaskItem/GetTaskResponse.cs
@@ -0,0 +1,19 @@
+﻿using WpfNotes.ApiModels.Category;
+
+namespace WpfNotes.ApiModels.TaskItem
+{
+    public class GetTaskResponse
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime? Notification { get; set; } = null; // in UTC
+
+        public GetCategoryResponse Category { get; set; }
+    }
+}
